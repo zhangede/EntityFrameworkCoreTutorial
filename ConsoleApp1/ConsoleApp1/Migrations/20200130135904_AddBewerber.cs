c73e764feaa3b3ca317b7bf4b781ecc888da244e
zhang <zhang7777@gmail.com>
2020-01-30 15:08:26 +0100

    add migration example

---
+++ ConsoleApp1/ConsoleApp1/Migrations/20200130135904_AddBewerber.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConsoleApp1.Migrations
+{
+    public partial class AddBewerber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into BewerberSet (Vorname, Discriminator) Values ('Marcor', 'Bewerber')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
